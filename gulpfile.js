06cbdc64f3cbceae7fdf8b1d49306d1d2ec88981
Bryan Sepia <bryanromero@gmail.com>
2022-08-12 14:54:03 -0500

    iniciando proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -63,4 +63,4 @@ function watchArchivos() {
 
 exports.css = css;
 exports.watchArchivos = watchArchivos;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos); 
+exports.default = parallel(css, javascript, imagenes, versionWebp, watchArchivos);      
